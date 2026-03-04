4591a92e96a9106b1fd54ff9bf3b97234751fda6
arturdanes <arturdanes@up.edu.ph>
2026-03-04 12:01:33 +0800

    chore: add placeholder files to track empty directories

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,18 @@
 import { defineConfig, globalIgnores } from "eslint/config";
+import next from "eslint-config-next";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import prettier from "eslint-config-prettier";
 
-const eslintConfig = defineConfig([
+export default defineConfig([
+  ...next,
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
+  prettier,
   globalIgnores([
-    // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
     "build/**",
     "next-env.d.ts",
   ]),
 ]);
-
-export default eslintConfig;
