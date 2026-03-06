e58899c15879edf53b1bc49f1a2d30af332f0ef7
tmbanes <tmbanes@up.edu.ph>
2026-03-06 20:53:59 +0800

    chore: updated cofnfig files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,39 @@
 import js from "@eslint/js";
-import prettier from "eslint-plugin-prettier";
+import tsParser from "@typescript-eslint/parser";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import reactPlugin from "eslint-plugin-react";
+import prettierPlugin from "eslint-plugin-prettier";
 import prettierConfig from "eslint-config-prettier";
 
 export default [
   js.configs.recommended,
 
   {
-    files: ["**/*.{js,ts}"],
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: "module",
+        ecmaFeatures: { jsx: true }
+      }
+    },
     plugins: {
-      prettier
+      "@typescript-eslint": tsPlugin,
+      react: reactPlugin,
+      prettier: prettierPlugin
     },
     rules: {
+      ...tsPlugin.configs.recommended.rules,
       "prettier/prettier": "error"
+    },
+    settings: {
+      react: {
+        version: "detect",   
+        pragma: "React",          
+        reactNamespace: "React",
+        jsxRuntime: "automatic"  
+      }
     }
   },
 
