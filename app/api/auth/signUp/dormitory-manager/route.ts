92ca32082a9b41fc6f7a639738576d41d50ead2f
franz . <fssaragena@up.edu.ph>
2026-03-08 22:52:21 +0800

    chore: separated signUp routes, added api/admin

--- app/api/auth/signUp/dormitory-manager/route.ts
+++ app/api/auth/signUp/dormitory-manager/route.ts
@@ -1,21 +1,14 @@
 import { NextRequest, NextResponse } from "next/server";
-import { signUpAsStudent } from "@/lib/auth";
+import { createDormitoryManager } from "@/lib/auth";
 
 async function POST(request: NextRequest) {
     try {
-
-       
         const userData = await request.json(); // get data from request body
-
-        // up mail validation
-        if(!userData.email.endswith('@up.edu.ph')) {
-            return NextResponse.json({ error: "Email must be a valid UP email address" }, { status: 400 }); 
-        }
-        const result = await signUpAsStudent(userData); // trigger sign-up function
+        const result = await createDormitoryManager(userData); // trigger sign-up function
         if (!result.success) { // if sign-up failed, return error response
             return NextResponse.json({ error: result.error }, { status: 400 });
         }
-        return NextResponse.json({ message: "Student created successfully" }, { status: 201 });
+        return NextResponse.json({ message: "Dorm manager created successfully" }, { status: 201 });
     } catch (error) {
         console.error('[ERROR] signing up with email:', error);
         return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
