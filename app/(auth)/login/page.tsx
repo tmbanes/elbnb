e0f92b7253717b1a2a99a9774182fc308ec67b8f
arturdanes <arturdanes@up.edu.ph>
2026-03-17 22:51:15 +0800

    feat: added login and signup with email and password page

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,3 +1,13 @@
-export default function LoginPage() {
-  return <div>Login</div>
+import { createSupabaseServerClient } from "@/lib/supabase/server-client";
+import SignUpWithEmailSetup from "./LoginWithEmailSetup";
+import GoogleLoginSetup from "../google-login/GoogleLoginSetup";
+
+export default async function LoginPage() {
+  const supabase = await createSupabaseServerClient()
+    const {
+      data: { user },
+    } = await supabase.auth.getUser();
+  
+    console.log( { user });
+    return <SignUpWithEmailSetup user={user} />;
 }
