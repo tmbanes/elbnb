07e071e91f310290828c04753a5281fde12c7d2d
vulnanx <mirano.christel28@gmail.com>
2026-03-29 00:50:04 +0800

    auth: ensure authentication before entering dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,16 @@
-export default function DashboardPage() {
-  return <div>Dashboard</div>;
+import { createSupabaseServerClient } from "@/lib/supabase/server-client";
+import { redirect } from "next/navigation";
+
+export default async function DashboardPage() {
+  const supabase = await createSupabaseServerClient();
+  const {
+    data: { session },
+    error,
+  } = await supabase.auth.getSession();
+
+  if (error || !session) {
+    redirect("/auth/");
+  }
+
+  return <div>Dashboard for {session.user.email}</div>;
 }
