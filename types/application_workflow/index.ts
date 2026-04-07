5243d22c25e14f902ed4ae23cb2410e54516f606
vulnanx <mirano.christel28@gmail.com>
2026-04-07 18:48:10 +0800

    refractor: added guard checks for applications for students and guest

--- types/application_workflow/index.ts
+++ types/application_workflow/index.ts
@@ -4,10 +4,14 @@ import { AccommodationAssignment } from '@/types/assignment_workflow';
 export type ApplicationStatus = "pending_dorm_manager" | "pending_admin" | "pending_payment" | "accepted" | "rejected" | "cancelled";
 export type DocumentType =  "university_id" | "form_5" | "proof_of_payment" | "parent_consent" | "other"
 export type DocumentStatus = "pending" | "verified" | "rejected"
+export type CancellableStatus = "pending_dorm_manager" | "pending_admin" | "pending_payment" 
+
 
 // BASED ON SUPABASE PROPERTIES + accommodation_assignment 
 export interface AccommodationApplication {
   application_id: string;
+  user_id: string;
+  unit_id: string;
   preferred_accommodation: string; // REQUIRED, accommodation_id.
   preferred_unit_type: string;
   date_submitted: string; // ISO date string
@@ -16,8 +20,6 @@ export interface AccommodationApplication {
   check_out: string; // ISO date string
   number_of_companions: number;
   application_status: ApplicationStatus;
-  user_id: string;
-  unit_id: string;
   accommodation_assignment?: AccommodationAssignment | null;
 }
 
