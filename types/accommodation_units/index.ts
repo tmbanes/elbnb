a0f4841a6ec751850bc816fec0065702b0956416
vulnanx <mirano.christel28@gmail.com>
2026-04-07 15:05:49 +0800

    refractor: added renting_space and dormitory propoerties

--- types/accommodation_units/index.ts
+++ types/accommodation_units/index.ts
@@ -8,8 +8,12 @@ export type AccommodationType = "dormitory" | "renting_space"
 export type UnitAccommodationStatus = "active" | "inactive";
 
 // RENTING_SPACE ACCOMMODATION ENUM TYPES
+export type PropertyType = "apartment" | "boarding" | "transient" | "house";
 
 // DORMITORY ACCOMMODATION ENUM TYPES
+export type TermType = "semestral" | "annual";
+export type College = "CAS" | "CEAT" | "CAFS" | "CVM" | "CDC" | "CEM" | "CHE" | "CFNR" | "SESAM" | "CPAf";
+export type DegreePrograms = "";
 
 // BASED ON SUPABASE PROPERTIES + vacant slots
 export interface Unit {
@@ -39,4 +43,25 @@ export interface Accommodation {
     accommodation_status: UnitAccommodationStatus;
     total_capacity: number;
     allowed_application: string; // ISO date string
+}
+
+// BASED ON SUPABASE PROPERTIES
+export interface Dormitory {
+    accommodation_id: string;
+    number_of_semestersAllowed: number;
+    curfew_time: string; // ISO time string
+    allowed_programs: College[]; // List of allowed colleges
+    term_type: TermType
+    separate_by_gender: boolean
+}
+
+// BASED ON SUPABASE PROPERTIES
+export interface RentingSpace {
+    accommodation_id: string
+    property_type: PropertyType
+    allow_shortterm_stay: boolean
+    allow_longterm_stay: boolean
+    minimum_stay_days: number
+    maximum_stay_days: number
+    security_deposit_required: boolean
 }
