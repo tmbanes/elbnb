edeb3b5a865d5d95af5ff806a9834aefb633aa69
arturdanes <arturdanes@up.edu.ph>
2026-04-10 21:06:02 +0800

    fix: resolve utils import and ui dependencies errors

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,5 +1,5 @@
 // app\admin\layout.tsx
-import { requireRole } from "@/lib/utils";
+import { requireRole } from "@/lib/utils/auth-utils";
 
 export default async function GuestLayout({ children }: { children: React.ReactNode }) {
     await requireRole(["housing_admin"]);
