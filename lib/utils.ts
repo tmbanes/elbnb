65406f5ce6b2673c8bbeb62df1321cc516ab9f24
Elisha <evclaveria@up.edu.ph>
2026-04-11 15:15:03 +0800

    fix: resolve util errors

--- lib/utils.ts
+++ lib/utils.ts
@@ -69,9 +69,3 @@ export async function redirectByRole() {
     }
     
 }
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
