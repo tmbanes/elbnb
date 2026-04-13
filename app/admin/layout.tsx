60b1908079807d88ec61a75ce0f24fcfbdff228c
ngvalentino <ngvalentino@up.edu.ph>
2026-04-13 17:47:36 +0800

    chore: added consistent bg color and bg text

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,7 +1,16 @@
 // app\admin\layout.tsx
-import { requireRole } from "@/lib/utils/auth-utils";
+import { requireRole } from "@/lib/utils/auth-utils"
 
-export default async function GuestLayout({ children }: { children: React.ReactNode }) {
-    await requireRole(["housing_admin"]);
-    return <>{children}</>;
+export default async function GuestLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  await requireRole(["housing_admin"])
+
+  return (
+    <div className="min-h-screen bg-[#F6F8D5] text-[#44291B] font-archivo">
+      {children}
+    </div>
+  )
 }
