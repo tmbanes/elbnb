c691f91b29f22fa20f299a42286ab00ea3e2d703
Laqui976 <rplaqui@up.edu.ph>
2026-04-21 09:30:54 +0800

    feat: reports notification

--- app/(auth)/role-selection/page.tsx
+++ app/(auth)/role-selection/page.tsx
@@ -1,14 +1,5 @@
-import { redirect } from "next/navigation";
 import RoleSelection from "./RoleSelection";
-import { getUserWithRole } from "@/lib/auth/client-auth";
-import { User } from "@/types/user.types";
 
-export default async function RoleSelectionPage() {
-  const user: User | null = await getUserWithRole();
-      
-  if (!user || !user.role) {
-    redirect(`/`);
-  }
-
+export default function RoleSelectionPage() {
   return <RoleSelection />;
 }
