b10186b7b40526eced1876b0d4a638700dd31787
djeanabriones <djeanabriones@gmail.com>
2026-04-24 20:49:50 +0800

    chore: integrated backend to student dashboard, added an extend stay functionality, added accommodations preview

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cywurzembhxgwqvpsrlh.supabase.co",
+      },
+      {
+        protocol: "https",
+        hostname: "international.uplb.edu.ph",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
