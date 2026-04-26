3297d73ae99917b5e839f1ff6cd709640fd078d9
tmbanes <tmbanes@up.edu.ph>
2026-04-26 19:09:29 +0800

    chore: deleted unusued files

--- test-db.js
+++
@@ -1,7 +0,0 @@
-const { createClient } = require('@supabase/supabase-js');
-const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
-async function run() {
-  const { data, error } = await supabase.from('accommodation_assignment').select('assignment_status');
-  console.log(data);
-}
-run();
