9a4fbdd4af59e50164b840c6562aa242bb875079
Elisha <evclaveria@up.edu.ph>
2026-04-27 13:55:06 +0800

    style: modify header in housing management manager's page

--- app/manager/housing/HousingContent.tsx
+++ app/manager/housing/HousingContent.tsx
@@ -2,8 +2,12 @@
 
 import { useState } from "react";
 import { Property } from "@/types/housing/types";
+import { Archivo, Archivo_Black } from "next/font/google";
 import HousingList from "./HousingList";
 import HousingDetail from "./HousingDetail";
+
+const archivo = Archivo({ subsets: ["latin"] });
+const archivoBlack = Archivo_Black({ subsets: ["latin"], weight: "400" });
 
 export default function HousingContent({ properties }: { properties: Property[] }) {
   const [selectedProperty, setSelectedProperty] = useState<Property | null>(null);
@@ -20,8 +24,8 @@ export default function HousingContent({ properties }: { properties: Property[] 
   return (
     <div className="space-y-6 animate-in fade-in duration-700">
       <div className="flex flex-col gap-2">
-        <h1 className="text-4xl font-black text-[#44291B] font-archivo-black tracking-tight">Housing Management</h1>
-        <p className="text-md text-[#44291B] font-medium">Manage and monitor your assigned properties and units</p>
+        <h1 className={`${archivoBlack.className} pt-6 text-3xl md:text-5xl text-[#44291B] mr-2`}>Housing Management</h1>
+        <p className="text-md text-[#44291B] font-medium">Manage and monitor your assigned property and units</p>
       </div>
 
       <HousingList
